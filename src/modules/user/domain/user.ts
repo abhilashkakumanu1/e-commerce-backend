785cf40e3e110d9ccab5a8ce646ff0a73b11f6b0
Abhilash Kakumanu <abhilashkakumanu1@gmail.com>
2022-08-25 09:09:59 +0530

    add user entity

---
+++ src/modules/user/domain/user.ts
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { Password } from "../../../shared/domain/ValueObjects/password";
+import { userDTO } from "../dto/user";
+
+interface UserProps {
+    id: string;
+    username: string;
+    password: Password;
+    type: "seller" | "buyer";
+    createdAt?: string | Date;
+    updatedAt?: string | Date;
+}
+
+export class User {
+    readonly props: UserProps;
+
+    // Private constructor => Singleton Pattern
+    private constructor(props: UserProps) {
+        this.props = props;
+    }
+
+    public static create(props: userDTO, id?: string): User {
+        // TODO: Do, any required validation here!
+
+        const password = Password.create({ value: props.password });
+
+        // create user if it doesn't exist
+        let userId = id;
+        if (!id) {
+            userId = uuidv4();
+        }
+
+        // Add defaults for createdAt, updatedAt if they are not present
+        props.createdAt ?? (props.createdAt = new Date());
+        props.updatedAt ?? (props.updatedAt = new Date());
+
+        const user = new User({ ...props, password, id: userId });
+
+        return user;
+    }
+}
